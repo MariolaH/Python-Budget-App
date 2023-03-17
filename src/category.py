2215c21cf7bbd8186f69bf0408597f43bdb028b2
MariolaH <mariola.hullings@student.awesomeinc.org>
2023-03-17 18:18:01 +0000

    works!

--- src/category.py
+++ src/category.py
@@ -3,22 +3,22 @@ class Category:
         self.name = name
         self.ledger = []
         self.balance = 0
-        self.description = ""
 
 
     def deposit(self, amount, description=''):
         self.ledger.append({"amount": amount, "description": description})
         self.balance += amount
-        print(f'{self.balance}.00 has been deposited into {self.name}')
-        print(self.ledger)
+        # print(f'{self.balance}.00 has been deposited into {self.name}')
+        # print(self.ledger)
 
     def withdraw(self, amount, description=''):
         # check to see if have right amount of funds
-        if(self.check_funds(amount)):
+        if self.check_funds(amount):
+            return False
+        else:
+            self.balance -= amount
             self.ledger.append({"amount": -abs(amount), "description": description})
             return True
-        else:
-            return False
     
     def get_balance(self):
         for amount in self.ledger:
@@ -26,37 +26,38 @@ class Category:
 
 # transfer from 1 category to another
     def transfer(self,amount, category):
-        if (self.check_funds(amount)):
-            self.withdraw(amount, "Transfer to " + category.name)
-            self.deposit(amount, "Transfer from" + self.name)
+        if self.check_funds(amount):
+            self.withdraw(amount, "Transfer to " + self.name)
+            self.deposit(amount, "Transfer from " + self.name)
             return True
         else:
             return False
  
     def check_funds(self, amount):
-        if amount < self.balance:
-            print("Save to deposit")
-            return True
+        if self.balance >= amount:
+            # print("Save to deposit")
+            return False
         else:
-            print("Insufficiant funds")
-            return False   
+            # print("Insufficiant funds")
+            return True  
 
 
     def __str__(self):
-        print(f'**********{name}********')
-        # print({self.ledger} + {self.balance})
-        # print({self.balance})
+        print(f"""********** {self.name} ********""")
+        for item in self.ledger:
+            print(f"{item['description']} :  $ {item['amount']}")
+        return f'TOTAL: {self.balance}'
 
 
 # CATERGORIES (name)
-initial_deposit = Category('Intial Deposit')
+initial_deposit = Category('BUDGET')
 food = Category('food')
 clothing = Category('clothing')
 entertainment = Category('entertainment')
 
 # deposit
 
-initial_deposit.deposit(1000, 'deposit')
+initial_deposit.deposit(1000, 'Deposit')
 
 # withdraw
 
@@ -69,4 +70,5 @@ initial_deposit.withdraw(75, 'food')
 
 initial_deposit.transfer(75, clothing)
 
+print(initial_deposit)
 
